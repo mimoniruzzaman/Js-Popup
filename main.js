2054b57105009fcbb7b01ce52e3cb27aa84a2812
Mi Moniruzzaman <118038390+mimoniruzzaman@users.noreply.github.com>
2024-05-08 20:52:35 -0700

    Update main.js

--- main.js
+++ main.js
@@ -22,3 +22,11 @@ document.querySelector(".close-icon").addEventListener("click", () => {
 document.querySelector(".overlay").addEventListener("click", () => {
   document.querySelector(".overlay").style.display = "none";
 });
+
+
+// document.getElementById("click-btn").style.color = "white";
+// document.getElementById("click-btn").style.backgroundColor = "green";
+// document.getElementById("click-btn").style.padding = "20px";
+
+// document.querySelector(".popup-body h1").style.backgroundColor = "green";
+// document.querySelector(".popup-body h1").style.height = "500px";
